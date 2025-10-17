8a19f3d2d0dcb4f5ca855ce8b05b6cf43badf115
jrohrer3 <jrohrer3@stumail.jccc.edu>
2025-10-16 22:33:21 -0500

    Update Click_script.js

--- Javascript5/Click_script.js
+++ Javascript5/Click_script.js
@@ -1,21 +1,6 @@
 // Joel Rohrer October 21 2025
 const gallery = document.querySelector('.gallery');
-const totalPanels = 25;
 
-// Add 25 images to the gallery
-for (let i = 1; i <= totalPanels; i++) {
-  const panel = document.createElement('div');
-  panel.classList.add('panel');
-
-  const img = document.createElement('img');
-  img.src = `images/image${i}.jpg`; // Make sure you have images 1-25
-  img.alt = `Image ${i}`;
-
-  panel.appendChild(img);
-  gallery.appendChild(panel);
-}
-
-// Click & Drag Scrolling
 let isDown = false;
 let startX;
 let scrollLeft;
@@ -41,6 +26,6 @@ gallery.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - gallery.offsetLeft;
-  const walk = (x - startX) * 2; // scroll-fast factor
+  const walk = (x - startX) * 2; // scroll speed
   gallery.scrollLeft = scrollLeft - walk;
 });
